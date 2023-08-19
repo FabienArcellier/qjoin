80e65d02f97df8d2f79b27566fcf30394934ab99
Fabien Arcellier <fabien.arcellier@gmail.com>
2023-08-19 09:32:38 +0200

    implement minimum valuable code to run the getting started
    * feat: the qjoin-on-join-all request is implemented with key argument as attribute name
    * feat: the qjoin-on-join-all request is implemented with key argument as function

--- src/qjoin/main.py
+++ src/qjoin/main.py
@@ -1,16 +1,103 @@
-from typing import Iterable, Any, Tuple, List
+import dataclasses
+from typing import Iterable, Any, Tuple, List, Union, Callable, Hashable, Optional
 
 import qjoin
+
+@dataclasses.dataclass
+class QjoinJoin:
+    """
+    Definition of a join in qjoin. We find there the collection to join, the field on which we perform the join.
+    """
+    collection: Iterable[Any]
+    key: Optional[Union[str, Callable[[Any], Hashable]]] = None
 
 
 class Qjoin:
 
     def __init__(self, collection: Iterable[Any]):
         self._base_collection = collection
+        self.join_definitions: List['QjoinJoin'] = []
 
     def __iter__(self):
+        predicates = []
+        for join_definition in self.join_definitions:
+            is_key_used = join_definition.key is not None
+            is_key_str = join_definition.key is not None and isinstance(join_definition.key, str)
+            is_key_func = join_definition.key is not None and callable(join_definition.key)
+
+            predicate = lambda elt_left, elt_right: False
+            if is_key_used and is_key_str:
+                predicate = lambda elt_left, elt_right: elt_left[join_definition.key] == elt_right[join_definition.key]
+            elif is_key_used and is_key_func:
+                predicate = lambda elt_left, elt_right: join_definition.key(elt_left) == join_definition.key(elt_right)
+
+            predicates.append(predicate)
+
         for element in self._base_collection:
-            yield (element, )
+            result = [element]
+            for join_definition_index, join_definition in enumerate(self.join_definitions):
+                join_match = False
+                for element_to_join in join_definition.collection:
+                    predicate = predicates[join_definition_index]
+                    if predicate(element, element_to_join):
+                        result.append(element_to_join)
+                        join_match = True
+                        break
+                if join_match is False:
+                    result.append(None)
+
+            yield tuple(result)
+
+    def join(self, collection: Iterable[Any], key: Union[str, Callable[[Any], Hashable]] = None) -> 'Qjoin':
+        """
+        Performs a join in a qjoin query with the base collection.
+
+        The first technique is to use a simple key as the join key.
+
+        >>> spacecrafts = [
+        >>>    {'name': 'Kepler', 'cospar_id': '2009-011A', 'satcat': 34380},
+        >>>    {'name': 'GRAIL (A)', 'cospar_id': '2011-046', 'satcat': 37801},
+        >>>    {'name': 'InSight', 'cospar_id': '2018-042a', 'satcat': 43457},
+        >>>    {'name': 'lucy', 'cospar_id': '2021-093A', 'satcat': 49328},
+        >>>    {'name': 'Psyche', 'cospar_id': None, 'satcat': None},
+        >>> ]
+        >>>
+        >>> global_spacecrafts = qjoin.on(spacecrafts).join(spacecrafts_properties, key='name')
+        >>> for spacecraft, spacecraft_properties in global_spacecrafts:
+        >>>     print(spacecraft['name'])
+        >>>     print('-' * len(spacecraft['name']))
+        >>>     print(spacecrafts_property['dimension'])
+        >>>     print('')
+        >>>     print('')
+
+        A second technique is to use a function to generate a more complex join key.
+        This function must return a hashable type in order to be used as a join key.
+
+        >>> spacecrafts = [
+        >>>    {'name': 'Kepler', 'cospar_id': '2009-011A', 'satcat': 34380},
+        >>>    {'name': 'GRAIL (A)', 'cospar_id': '2011-046', 'satcat': 37801},
+        >>>    {'name': 'InSight', 'cospar_id': '2018-042a', 'satcat': 43457},
+        >>>    {'name': 'lucy', 'cospar_id': '2021-093A', 'satcat': 49328},
+        >>>    {'name': 'Psyche', 'cospar_id': None, 'satcat': None},
+        >>> ]
+        >>>
+        >>> global_spacecrafts = qjoin.on(spacecrafts).join(spacecrafts_properties, key=lambda s: s['name'].lower())
+        >>> for spacecraft, spacecraft_properties in global_spacecrafts:
+        >>>     print(spacecraft['name'])
+        >>>     print('-' * len(spacecraft['name']))
+        >>>     print(spacecrafts_property['dimension'])
+        >>>     print('')
+        >>>     print('')
+
+        The join function is lazy. Until a render function is called like .all or a loop is used on the QJoin instance,
+        the join is just declared.
+        """
+        if key is None:
+            raise ValueError('A key has to be specified when using join in qjoin query. qjoin.join(key="mykey") or qjoin.join(key=lambda x: x.mykey)')
+
+        join = QjoinJoin(collection, key)
+        self.join_definitions.append(join)
+        return self
 
     def all(self) -> List[Tuple[Any, ...]]:
         """
--- tests/test_qjoin.py
+++ tests/test_qjoin.py
@@ -1,3 +1,5 @@
+import pytest
+
 import qjoin
 
 
@@ -35,3 +37,77 @@ def tests_qjoin_all_request_should_return_a_list():
     spacecraft_global = qjoin.on(spacecrafts).all()
     assert isinstance(spacecraft_global, list)
     assert len(spacecraft_global) == len(spacecrafts)
+
+
+def tests_qjoin_join_request_should_join_2_collection_on_a_simple_key():
+    """
+    tests that the join function joins 2 collections on a simple key
+    """
+    spacecrafts = [
+        {'name': 'Kepler', 'cospar_id': '2009-011A', 'satcat': 34380},
+        {'name': 'GRAIL (A)', 'cospar_id': '2011-046', 'satcat': 37801},
+        {'name': 'InSight', 'cospar_id': '2018-042a', 'satcat': 43457},
+        {'name': 'lucy', 'cospar_id': '2021-093A', 'satcat': 49328},
+        {'name': 'Psyche', 'cospar_id': None, 'satcat': None},
+    ]
+
+    spacecraft_properties = [
+        {'name': 'Kepler', 'dimension': (4.7, 2.7, None), 'power': 1100, 'launch_mass': 1052.4},
+        {'name': 'GRAIL (A)', 'launch_mass': 202.4},
+        {'name': 'InSight', 'dimension': (6, 1.56, 1), 'power': 600, 'launch_mass': 694},
+        {'name': 'lucy', 'dimension': (13, None, None), 'power': 504, 'launch_mass': 1550},
+    ]
+
+    spacecraft_global = qjoin.on(spacecrafts).join(spacecraft_properties, key='name').all()
+    assert spacecraft_global[0] == (spacecrafts[0], spacecraft_properties[0])
+    assert spacecraft_global[3] == (spacecrafts[3], spacecraft_properties[3])
+
+
+def tests_qjoin_join_request_should_join_2_collection_on_function():
+    """
+    tests that the join function joins 2 collections on a function
+    """
+    spacecrafts = [
+        {'name': 'Kepler', 'cospar_id': '2009-011A', 'satcat': 34380},
+        {'name': 'GRAIL (A)', 'cospar_id': '2011-046', 'satcat': 37801},
+        {'name': 'InSight', 'cospar_id': '2018-042a', 'satcat': 43457},
+        {'name': 'lucy', 'cospar_id': '2021-093A', 'satcat': 49328},
+        {'name': 'Psyche', 'cospar_id': None, 'satcat': None},
+    ]
+
+    spacecraft_properties = [
+        {'name': 'Kepler', 'dimension': (4.7, 2.7, None), 'power': 1100, 'launch_mass': 1052.4},
+        {'name': 'GRAIL (A)', 'launch_mass': 202.4},
+        {'name': 'InSight', 'dimension': (6, 1.56, 1), 'power': 600, 'launch_mass': 694},
+        {'name': 'lucy', 'dimension': (13, None, None), 'power': 504, 'launch_mass': 1550},
+    ]
+
+    spacecraft_global = qjoin.on(spacecrafts).join(spacecraft_properties, key=lambda s: s['name'].lower()).all()
+    assert spacecraft_global[0] == (spacecrafts[0], spacecraft_properties[0])
+    assert spacecraft_global[3] == (spacecrafts[3], spacecraft_properties[3])
+
+
+def tests_qjoin_join_request_should_fail_when_there_is_no_key_and_left_right_join():
+    """
+    tests that the join function joins 2 collections on a function
+    """
+    spacecrafts = [
+        {'name': 'Kepler', 'cospar_id': '2009-011A', 'satcat': 34380},
+        {'name': 'GRAIL (A)', 'cospar_id': '2011-046', 'satcat': 37801},
+        {'name': 'InSight', 'cospar_id': '2018-042a', 'satcat': 43457},
+        {'name': 'lucy', 'cospar_id': '2021-093A', 'satcat': 49328},
+        {'name': 'Psyche', 'cospar_id': None, 'satcat': None},
+    ]
+
+    spacecraft_properties = [
+        {'name': 'Kepler', 'dimension': (4.7, 2.7, None), 'power': 1100, 'launch_mass': 1052.4},
+        {'name': 'GRAIL (A)', 'launch_mass': 202.4},
+        {'name': 'InSight', 'dimension': (6, 1.56, 1), 'power': 600, 'launch_mass': 694},
+        {'name': 'lucy', 'dimension': (13, None, None), 'power': 504, 'launch_mass': 1550},
+    ]
+
+    try:
+        spacecraft_global = qjoin.on(spacecrafts).join(spacecraft_properties).all()
+        pytest.xfail('expected ValueError execption because there is no key and no left_right_join')
+    except ValueError as exception:
+        assert True
